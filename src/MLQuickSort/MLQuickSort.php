3f2d12b8b059834531724288273b64e84a5fe182
macho <wmlzy6@163.com>
2022-01-12 09:49:53 +0800

    更新包 修复引入问题

--- src/MLQuickSort/MLQuickSort.php
+++ src/MLQuickSort/MLQuickSort.php
@@ -6,10 +6,8 @@
  * Created on 2021/12/24
  * Class QuickSort
  */
-
-namespace QuickSort;
-class MLQuickSort
-{
+namespace QuickSort\MLQuickSort;
+class MLQuickSort{
     /**
      * Name 小值变大值
      * Created by WML
@@ -19,18 +17,18 @@ class MLQuickSort
      * @param array $arr
      * @return array
      */
-    static function minToMax($old = [], $new = [], $arr = [])
+    static function minToMax($old=[],$new=[],$arr=[])
     {
         $keep = [];
-        foreach ($arr as $value) {
-            if ($value['id'] == $old['id']) {
-                $keep[] = ['id' => $value['id'], 'order' => $new['order']];
+        foreach ($arr as $value){
+            if($value['id'] == $old['id']){
+                $keep[] = ['id' => $value['id'],'order' => $new['order']];
             }
-            if (($value['order'] <= $new['order']) && ($value['order']) >= $old['order']) {
-                if ($value['id'] == $old['id']) continue;
+            if(($value['order'] <= $new['order']) && ($value['order']) >= $old['order']){
+                if($value['id'] == $old['id']) continue;
                 $new_order = $value['order'] - 1;
-                $keep[] = ['id' => $value['id'], 'order' => $new_order];
-            } else {
+                $keep[] = ['id' => $value['id'],'order' => $new_order];
+            }else{
                 $keep[] = $value;
             }
         }
@@ -38,18 +36,18 @@ class MLQuickSort
     }
 
     //大值变小值
-    static function maxToMin($old = [], $new = [], $arr = [])
+    static function maxToMin($old=[],$new=[],$arr=[])
     {
         $keep = [];
-        foreach ($arr as $value) {
-            if ($value['id'] == $old['id']) {
-                $keep[] = ['id' => $value['id'], 'order' => $new['order']];
+        foreach ($arr as $value){
+            if($value['id'] == $old['id']){
+                $keep[] = ['id' => $value['id'],'order' => $new['order']];
             }
-            if (($value['order'] >= $new['order']) && ($value['order']) <= $old['order']) {
-                if ($value['id'] == $old['id']) continue;
+            if(($value['order'] >= $new['order']) && ($value['order']) <= $old['order']){
+                if($value['id'] == $old['id']) continue;
                 $new_order = $value['order'] + 1;
-                $keep[] = ['id' => $value['id'], 'order' => $new_order];
-            } else {
+                $keep[] = ['id' => $value['id'],'order' => $new_order];
+            }else{
                 $keep[] = $value;
             }
         }
@@ -72,6 +70,7 @@ class MLQuickSort
 //['id' => 3,'order' => 2]
 //['id' => 4,'order' => 4]
 //['id' => 5,'order' => 5]
+
 
 
 //return (new QuickSort)->maxToMin(['id' => 4,'order' => 4],['id' => 4,'order' => 2],$arr);
